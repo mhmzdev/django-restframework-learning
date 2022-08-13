98d6d68ceeeb1444622749808379493afb4228db
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2022-08-13 20:49:53 +0500

    Django project setup

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import api_home
-
-urlpatterns = [
-    path('', api_home)
-]
--- api/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-def api_home(request, *args, **kwargs):
-    return JsonResponse(
-        {
-            "message": 'You are doing well!!',
-        }
-    )
