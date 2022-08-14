331eb16be72ea8c550a709b07d901b3833891362
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2022-08-14 10:10:07 +0500

    handling json body and query params

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 I've put all the files in sequence in the branches while I'm learning to use django as backend framework.
 
-### 1st
+### 1st - Creating Django project
 - Created django project using command `django-admin startproject learning`
 
-### 2nd
+### 2nd - First API view/response
 - Created first api view, or we also call it api response
 - Create app name `api` via command `python manage.py startapp api`
 - return json response from url `http://localhost:8000/home/`
 
-### 3rd
+### 3rd - Returning back Data
 - Sending Json data from client side to server and handling it.
 - More like we send `body` in a `POST` request or `QueryParams`
+- Adding data like `headers` and `content_type`
--- api/views.py
+++ api/views.py
@@ -1,10 +1,23 @@
 import imp
+import json
 from django.http import JsonResponse
+
 
 # Create your views here.
 def home(request, *args, **kwargs):
-    return JsonResponse(
-        {
-            "message": "You are doing great so far!"
-        }
-    )
+    # request --> from HttpRequest(django) not from requests(python)
+
+    body = request.body  # byte string of JSON data
+    # we need to convert it from byte string to JSON
+
+    data = {}  # this is actual object/json/dict etc.
+    try:
+        data = json.loads(body) # json serialization
+    except:
+        pass
+    
+    data['params'] = dict(request.GET) # getting the query params http://localhost:8000/home/?some_key=some_value
+    data['headers'] = dict(request.headers) # headers needs to be json serialize here
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
